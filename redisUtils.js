08d26f3f4e4ec8fa9d0da4ff0488f6a0a6d2f11c
Avinash Bhardwaj <avinashb97@gmail.com>
2019-08-11 06:57:01 +0530

    linting fixes

--- redisUtils.js
+++ redisUtils.js
@@ -1,8 +1,9 @@
 const redis = require('redis');
+
 const client = redis.createClient();
 
-client.on("error", function (err) {
-    console.log("Error " + err);
+client.on('error', (err) => {
+    console.log(`Error ${err}`);
 });
 
 module.exports = client;
